e6043315f59b1ec8a8d90b4db70379254d32a454
j <101863806+pienetsammakot@users.noreply.github.com>
2022-04-05 00:16:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Jonathan Haavisto");
+Console.WriteLine("Teht 11");
+Console.WriteLine("--------------------");
+Console.WriteLine();
+
+List<int> luvut = new List<int>();
+
+for(int i = 0; i < 5; i++)
+{
+    Console.Write("Listan " + (i + 1) + ". luku? ");
+    luvut.Add(Convert.ToInt32(Console.ReadLine()));
+    Console.WriteLine();
+}
+
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine(luvut[i]);
+}
